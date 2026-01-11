b528d2ab99359d84c50ef79b44e5728ba316b47d
xXJanisXx <Janis.Minecrafto@gmx.de>
2026-01-11 15:15:59 +0100

    feat: update to 1.21.11

--- custom-names-plugin/src/main/kotlin/space/chunks/customname/plugin/CustomNameImpl.kt
+++ custom-names-plugin/src/main/kotlin/space/chunks/customname/plugin/CustomNameImpl.kt
@@ -3,7 +3,6 @@ package space.chunks.customname.plugin
 import net.kyori.adventure.text.Component
 import net.minecraft.network.protocol.Packet
 import net.minecraft.network.protocol.game.ClientGamePacketListener
-import org.bukkit.Bukkit
 import org.bukkit.craftbukkit.entity.CraftEntity
 import org.bukkit.craftbukkit.entity.CraftPlayer
 import org.bukkit.entity.Entity
@@ -11,7 +10,6 @@ import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
 import org.bukkit.scheduler.BukkitTask
-import org.jetbrains.annotations.Nullable
 import space.chunks.customname.api.CustomName
 import space.chunks.customname.plugin.util.SkeletonInteraction
 import java.util.function.Consumer
@@ -23,38 +21,34 @@ class CustomNameImpl(
 
     private val interaction = SkeletonInteraction(this)
 
-    // Target entity constants
     private var effectiveHeight = 0.0
     private var passengerOffset = 0.0
-
-    // Custom name constants
     private var nametagEntityId = 0
-
-    // States
     private var targetEntitySneaking = false
-
-    @Nullable
-    private var nameCallback: ((viewer: Player) -> Component?) = { null }
     private var hidden = false
 
+    private var nameCallback: (viewer: Player) -> Component? = { null }
     private var task: BukkitTask? = null
 
     init {
-        this.nametagEntityId = Bukkit.getUnsafe().nextEntityId()
+        this.nametagEntityId = net.minecraft.world.entity.Entity.nextEntityId()
 
-        val nmsEntity: net.minecraft.world.entity.Entity = (targetEntity as CraftEntity).handle
-        val ridingOffset = nmsEntity.getPassengerRidingPosition(null).subtract(nmsEntity.position()).y
-        val nametagOffset: Float = nmsEntity.type.dimensions.height + 0.5f
+        val nmsEntity = (targetEntity as CraftEntity).handle
 
-        // First, negate the riding offset to get to the bounding of the entity's bounding box
-        // Negate the natural nametag offset of interaction entities (0.5)
-        // Add the actual offset of the nametag
+        val ridingOffset = nmsEntity
+            .getPassengerRidingPosition(nmsEntity)
+            .subtract(nmsEntity.position()).y
+
+        val nametagOffset = nmsEntity.type.dimensions.height + 0.5f
+
         this.effectiveHeight = -ridingOffset - 0.5 + nametagOffset
         this.passengerOffset = ridingOffset
 
         this.task = object : BukkitRunnable() {
             override fun run() {
-                val riderPacket: Packet<ClientGamePacketListener> = this@CustomNameImpl.interaction.getRiderPacket()
+                val riderPacket: Packet<ClientGamePacketListener> =
+                    this@CustomNameImpl.interaction.getRiderPacket()
+
                 for (player in targetEntity.trackedPlayers) {
                     (player as CraftPlayer).handle.connection.send(riderPacket)
                 }
@@ -72,63 +66,38 @@ class CustomNameImpl(
         this.syncData()
     }
 
-    fun sendToClient(entity: Player) {
-        if (this.hidden) {
-            return
+    override fun setHidden(hidden: Boolean) {
+        this.hidden = hidden
+        this.runOnTrackers { player ->
+            if (hidden) removeFromClient(player) else sendToClient(player)
         }
+    }
 
-        (entity as CraftPlayer).handle.connection.send(interaction.initialSpawnPacket(entity))
+    fun sendToClient(entity: Player) {
+        if (!hidden) {
+            (entity as CraftPlayer).handle.connection
+                .send(interaction.initialSpawnPacket(entity))
+        }
     }
 
     fun removeFromClient(entity: Player) {
         (entity as CraftPlayer).handle.connection.send(interaction.removePacket())
     }
 
-    override fun setHidden(hidden: Boolean) {
-        this.hidden = hidden
-        this.runOnTrackers { player ->
-            if (hidden) {
-                this.removeFromClient(player)
-            } else {
-                this.sendToClient(player)
-            }
-        }
-    }
+    override fun getName(viewer: Player): Component? = nameCallback(viewer)
+    override fun getNametagId(): Int = nametagEntityId
+    override fun getTargetEntity(): Entity = targetEntity
+    override fun isTargetEntitySneaking(): Boolean = targetEntitySneaking
+    override fun getEffectiveHeight(): Double = effectiveHeight
+    override fun getPassengerOffset(): Double = passengerOffset
+    override fun isHidden(): Boolean = hidden
 
-    @Nullable
-    override fun getName(viewer: Player): Component? {
-        return this.nameCallback(viewer)
-    }
-
-    override fun getNametagId(): Int {
-        return this.nametagEntityId
-    }
-
-    override fun getTargetEntity(): Entity {
-        return this.targetEntity
-    }
-
-    override fun isTargetEntitySneaking(): Boolean {
-        return this.targetEntitySneaking
-    }
-
-    override fun getEffectiveHeight(): Double {
-        return this.effectiveHeight
-    }
-
-    override fun getPassengerOffset(): Double {
-        return this.passengerOffset
-    }
-
-    // Utilities
     private fun syncData() {
-        if (this.hidden) {
-            return
-        }
+        if (hidden) return
 
-        this.runOnTrackers { player ->
-            val dataPacket: Packet<ClientGamePacketListener> = interaction.syncDataPacket(player)
-            (player as CraftPlayer).handle.connection.send(dataPacket)
+        runOnTrackers { player ->
+            val packet = interaction.syncDataPacket(player)
+            (player as CraftPlayer).handle.connection.send(packet)
         }
     }
 
@@ -139,10 +108,6 @@ class CustomNameImpl(
     }
 
     fun close() {
-        task!!.cancel()
-    }
-
-    override fun isHidden(): Boolean {
-        return this.hidden
+        task?.cancel()
     }
 }
--- custom-names-plugin/src/main/kotlin/space/chunks/customname/plugin/util/DataAccessors.kt
+++ custom-names-plugin/src/main/kotlin/space/chunks/customname/plugin/util/DataAccessors.kt
@@ -14,32 +14,33 @@ object DataAccessors {
     private val reflectionRemapper = ReflectionRemapper.forReobfMappingsInPaperJar()
 
     val DATA_SHARED_FLAGS_ID: EntityDataAccessor<Byte> =
-        get(reflectionRemapper, Entity::class.java, "DATA_SHARED_FLAGS_ID")
+        get(Entity::class.java, "DATA_SHARED_FLAGS_ID")
 
-    val DATA_POSE: EntityDataAccessor<Pose> = get(reflectionRemapper, Entity::class.java, "DATA_POSE")
+    val DATA_POSE: EntityDataAccessor<Pose> =
+        get(Entity::class.java, "DATA_POSE")
 
     val DATA_CUSTOM_NAME: EntityDataAccessor<Optional<Component>> =
-        get(reflectionRemapper, Entity::class.java, "DATA_CUSTOM_NAME")
+        get(Entity::class.java, "DATA_CUSTOM_NAME")
 
     val DATA_CUSTOM_NAME_VISIBLE: EntityDataAccessor<Boolean> =
-        get(reflectionRemapper, Entity::class.java, "DATA_CUSTOM_NAME_VISIBLE")
+        get(Entity::class.java, "DATA_CUSTOM_NAME_VISIBLE")
 
     // Interaction entity
 
-    val DATA_WIDTH_ID: EntityDataAccessor<Float> = get(reflectionRemapper, Interaction::class.java, "DATA_WIDTH_ID")
+    val DATA_WIDTH_ID: EntityDataAccessor<Float> = get(Interaction::class.java, "DATA_WIDTH_ID")
 
-    val DATA_HEIGHT_ID: EntityDataAccessor<Float> = get(reflectionRemapper, Interaction::class.java, "DATA_HEIGHT_ID")
+    val DATA_HEIGHT_ID: EntityDataAccessor<Float> = get(Interaction::class.java, "DATA_HEIGHT_ID")
 
-    private fun <T> get(reflectionRemapper: ReflectionRemapper, clazz: Class<*>, name: String): EntityDataAccessor<T> {
+    private fun <T : Any> get(clazz: Class<*>, name: String): EntityDataAccessor<T> {
         try {
             return MethodHandles.privateLookupIn(clazz, MethodHandles.lookup())
                 .findStaticGetter(
-                    clazz, reflectionRemapper.remapFieldName(clazz, name),
+                    clazz,
+                    reflectionRemapper.remapFieldName(clazz, name),
                     EntityDataAccessor::class.java
                 ).invoke() as EntityDataAccessor<T>
         } catch (e: Throwable) {
             throw RuntimeException(e)
         }
     }
-
 }
